9ee531cf49395ea193acbeeb58eec98faeb764c9
studiousgamer <natyavidhanbiswas10@gmail.com>
2024-08-07 14:13:04 +0530

    writing

--- main.py
+++ main.py
@@ -5,13 +5,20 @@ pygame.display.set_caption("Ganit AI")
 running = True
 
 canvas = pygame.Surface((800, 700))
-
+writing = []
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
+    mouse_down = pygame.mouse.get_pressed()[0]
+    if mouse_down:
+        pos = pygame.mouse.get_pos()
+        writing.append(pos)
+
     screen.fill((0, 0, 0))
     canvas.fill((25, 25, 25))
+    for pos in writing:
+        pygame.draw.circle(canvas, (255, 255, 255), pos, 1)
     screen.blit(canvas, (0, 0))
     pygame.display.update()
